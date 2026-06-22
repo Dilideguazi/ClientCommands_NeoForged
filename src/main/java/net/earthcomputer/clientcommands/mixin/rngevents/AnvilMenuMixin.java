5cbbab75784eccfb259752ccc6a6263aa98dd1b8
joe <burtonjae@hotmail.co.uk>
2026-06-22 15:48:47 +0100

    Fix RNG events possibly being called on the server. Fixes #797

--- src/main/java/net/earthcomputer/clientcommands/mixin/rngevents/AnvilMenuMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/rngevents/AnvilMenuMixin.java
@@ -1,6 +1,7 @@
 package net.earthcomputer.clientcommands.mixin.rngevents;
 
 import net.earthcomputer.clientcommands.features.PlayerRandCracker;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.ItemStack;
@@ -14,7 +15,7 @@ public class AnvilMenuMixin {
 
     @Inject(method = "onTake", at = @At("HEAD"))
     public void onAnvilUse(Player player, ItemStack stack, CallbackInfo ci) {
-        if (!player.getAbilities().instabuild) {
+        if (player instanceof LocalPlayer && !player.getAbilities().instabuild) {
             PlayerRandCracker.onAnvilUse();
         }
     }
--- src/main/java/net/earthcomputer/clientcommands/mixin/rngevents/CrossbowItemMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/rngevents/CrossbowItemMixin.java
@@ -1,7 +1,9 @@
 package net.earthcomputer.clientcommands.mixin.rngevents;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.earthcomputer.clientcommands.features.PlayerRandCracker;
 import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CrossbowItem.class)
 public class CrossbowItemMixin {
     @Inject(method = "performShooting", at = @At("HEAD"))
-    private void onPerformShooting(CallbackInfo ci) {
-        PlayerRandCracker.onCrossbowUse();
+    private void onPerformShooting(CallbackInfo ci, @Local(argsOnly = true, name = "level") Level level) {
+        if (level.isClientSide()) {
+            PlayerRandCracker.onCrossbowUse();
+        }
     }
 }
