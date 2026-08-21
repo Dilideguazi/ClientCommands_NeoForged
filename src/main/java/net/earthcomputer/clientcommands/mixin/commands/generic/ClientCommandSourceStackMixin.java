9348836b6ddad91bfe3160d506efa8c24d96af77
Dilideguazi <pxc628142122022@outlook.com>
2026-08-21 19:58:10 +0800

    initial commit

--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ClientCommandSourceStackMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ClientCommandSourceStackMixin.java
@@ -5,15 +5,6 @@ import com.mojang.brigadier.suggestion.Suggestion;
 import net.earthcomputer.clientcommands.command.Flag;
 import net.earthcomputer.clientcommands.interfaces.IClientSuggestionsProvider;
 import net.earthcomputer.clientcommands.interfaces.IClientSuggestionsProvider_Alias;
-import net.minecraft.commands.CommandSource;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.permissions.PermissionSet;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.client.ClientCommandSourceStack;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,16 +16,12 @@ import java.util.Set;
 
 // Neo Edit: throws ClassCastException when executing commands
 @Mixin(ClientCommandSourceStack.class)
-public abstract class ClientCommandSourceStackMixin extends CommandSourceStack implements IClientSuggestionsProvider, IClientSuggestionsProvider_Alias {
+public class ClientCommandSourceStackMixin implements IClientSuggestionsProvider, IClientSuggestionsProvider_Alias {
     @Unique
     private final Set<String> seenAliases = new HashSet<>();
 
     @Unique
     private ImmutableMap<Flag<?>, Object> flags = ImmutableMap.of();
-
-    public ClientCommandSourceStackMixin(CommandSource source, Vec3 position, Vec2 rotation, ServerLevel level, PermissionSet permissions, String textName, Component displayName, MinecraftServer server, @Nullable Entity entity) {
-        super(source, position, rotation, level, permissions, textName, displayName, server, entity);
-    }
 
     @SuppressWarnings("unchecked")
     @Override
